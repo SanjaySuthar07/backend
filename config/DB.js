9c127825d127a204ec726dfda3c32fcca825e835
SanjaySuthar07 <sanjaysuthar121212@gmail.com>
2026-02-16 17:36:37 +0530

    db update

--- config/DB.js
+++ config/DB.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose")
 const connectDb = async () => {
     try {
-        await mongoose.connect(process.env.MONGODB_URI)
+        await mongoose.connect("mongodb+srv://sanjaysuthar123:dadu12gajjar@cluster0.gzkxn3i.mongodb.net/milk-managment")
     } catch (error) {
         console.log("server error ", error)
     }
